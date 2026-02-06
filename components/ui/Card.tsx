d8d8e2cf8255d38e6b550a0abadc9909807f56b9
tripathisaurabh <tripathisaurabh332@gmail.com>
2026-02-07 03:29:36 +0530

    Allow Card to accept standard div props

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -3,8 +3,12 @@ import { ReactNode } from "react";
 type CardProps = {
   children: ReactNode;
   className?: string;
-};
+} & React.HTMLAttributes<HTMLDivElement>;
 
-export default function Card({ children, className = "" }: CardProps) {
-  return <div className={`card p-6 ${className}`}>{children}</div>;
+export default function Card({ children, className = "", ...rest }: CardProps) {
+  return (
+    <div className={`card p-6 ${className}`} {...rest}>
+      {children}
+    </div>
+  );
 }
